ba8c8407c325c1b29549cf57afbede1eab4394c2
Abdullah Ibne Tayeb Tamur <109722082+bintamur@users.noreply.github.com>
2022-08-20 22:41:20 +0600

    Add files via upload

---
+++ Hello with.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+//#include<conio.h> to use getch()
+//no need of #include<conio.h> if i use getchar() .....both work same reason
+using namespace std;
+int main()
+{
+    cout<<"Hello World"<<endl;
+
+getchar();
+}
