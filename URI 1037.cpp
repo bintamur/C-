b79377a37e5116580824a83549f52bea3a35576c
Abdullah Ibne Tayeb Tamur <109722082+bintamur@users.noreply.github.com>
2022-08-21 20:10:37 +0600

    Update URI 1037.cpp

--- URI 1037.cpp
+++ URI 1037.cpp
@@ -1,27 +1,31 @@
-#include<bits/stdc++.h>
-
+#include<iostream>
 using namespace std;
-int main()
+
+int main ()
 {
- float n;
- cin>>n;
- if(n>=0 && n<=25)
- {
-     cout<<"Intervalo [0,25]"<<endl;
- }
- if(n>25 && n<=50)
- {
-     cout<<"Intervalo (25,50]"<<endl;
- }
- if(n>50 && n<=75)
- {
-     cout<<"Intervalo (50,75]"<<endl;
- }
- if(n>75 && n<=100)
- {
-     cout<<"Intervalo (75,100]"<<endl;
- }
- getchar();
+    double n;
+    cin>>n;
 
+    if(n>=0 && n<=25)
+    {
+        cout<<"Intervalo [0,25]\n";
+    }
+    else if(n>25 && n<=50)
+    {
+        cout<<"Intervalo (25,50]\n";
+    }
+    else if(n>50 && n<=75)
+    {
+        cout<<"Intervalo (50,75]\n";
+    }
+    else if(n>75 && n<=100)
+    {
+        cout<<"Intervalo (75,100]\n";
+    }
+    else if(n<0 || n>100)
+    {
+        cout<<"Fora de intervalo\n";
+    }
+
+    return 0;
 }
-
