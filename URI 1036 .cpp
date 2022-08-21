43ae68602183beb2a407d9cb2e05606e9a5bedfc
Abdullah Ibne Tayeb Tamur <109722082+bintamur@users.noreply.github.com>
2022-08-21 20:15:20 +0600

    Update URI 1036 .cpp

--- URI 1036 .cpp
+++ URI 1036 .cpp
@@ -5,20 +5,21 @@ int main()
 {
  double a,b,c,n,R1,R2;
  cin>>a>>b>>c;
- n=(b*b)-4*a*c;
- if(n>0)
+ 
+ if(a==0 || (b*b-(4*a*c)<0))
  {
-     cout<<fixed<<setprecision(5);
-     R1=(-b+sqrt(n))/2*a;
-     cout<<"R1="<<R1<<endl;
-     R2=(-b-sqrt(n))/2*a;
-     cout<<"R2="<< R2<<endl;
+  cout<<"Impossivel calcular\n";
  }
  else
  {
-     cout<<"Impossivel calcular"<<endl;
+
+ cout<<fixed<<setprecision(5);
+     R1=(-b+(sqrt(b*b-(4*a*c))))/(2*a);
+     cout<<"R1 = "<<R1<<endl;
+     R2=(-b-(sqrt(b*b-(4*a*c))))/(2*a);
+     cout<<"R2 = "<< R2<<endl;
  }
 
- getchar();
+getchar();
 
 }
